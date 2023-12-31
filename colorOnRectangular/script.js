8926b691fe607d4374d8ad3b4fd1a49baa4dda8c
mahmudtitumir <titumir947@gmail.com>
2024-01-01 01:57:44 +0600

    vanilla js added

--- colorOnRectangular/script.js
+++ colorOnRectangular/script.js
@@ -1,3 +1,5 @@
+/*
+// sovle using gsap libery for mapping rgb color
 let rectangle = document.getElementById("rectangle");
 
 rectangle.addEventListener("mouseover", function (position) {
@@ -33,4 +35,28 @@ rectangle.addEventListener("mouseleave", function () {
   gsap.to(rectangle, {
     backgroundColor: "white",
   });
+});
+*/
+
+//
+let rectangle = document.querySelector("#rectangle");
+const { left, right } = rectangle.getBoundingClientRect();
+
+rectangle.addEventListener("mousemove", (position) => {
+  let DistanceFromCenter = (left + right) / 2 - position.clientX;
+
+  let distanceInPercentage =
+    (DistanceFromCenter / (right - (left + right) / 2)) * 100;
+
+  let percentageForColor = 255 * (distanceInPercentage / 100);
+
+  if (position.clientX <= (left + right) / 2) {
+    rectangle.style.backgroundColor = `rgb(${percentageForColor},0,0)`;
+  } else {
+    rectangle.style.backgroundColor = `rgb(0,0,${percentageForColor * -1})`;
+  }
+});
+
+rectangle.addEventListener("mouseleave", function () {
+  rectangle.style.backgroundColor = "white";
 });
