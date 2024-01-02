52df6360ce525b41c1c0fe2dce78b198d5941e9a
mahmudtitumir <titumir947@gmail.com>
2024-01-03 03:37:30 +0600

    js finish

--- moveingRactangular/script.js
+++ moveingRactangular/script.js
@@ -1,32 +1,13 @@
-let cursor = document.querySelector("body");
-let rectangle = document.getElementById("#rectangle");
-/*
-cursor.addEventListener("mousemove", function (position) {
-  if (position.x > this.innerWidth / 2) {
-    rectangle.style.left = ` ${
-      window.innerWidth / 2 + Math.pow(positionx - window.innerWidth / 2, 0.9)
-    }px`;
-  } else {
-    rectangle.style.right = `${
-      window.innerWidth / 2 - Math.pow(window.innerWidth / 2 - event.x, 0.9)
-    }px`;
-  }
-  // } else {
-  //   rectangle.style.right = `${
-  //     window.innerWidth / 2 - Math.pow(position.x - window.innerWidth / 2, 0.9)
-  //   }px`;
-  // }
-});
-*/
-cursor.addEventListener("mousemove", (event) => {
-  console.log(event.x);
+let rectangle = document.getElementById("rectangle");
+function moving(event) {
   if (event.x > innerWidth / 2)
     rectangle.style.left = `${
-      window.innerWidth / 2 + Math.pow(event.x - window.innerWidth / 2, 0.9)
+      window.innerWidth / 2 + (event.x - window.innerWidth / 2)
     }px`;
   else {
     rectangle.style.left = `${
-      window.innerWidth / 2 - Math.pow(window.innerWidth / 2 - event.x, 0.9)
+      window.innerWidth / 2 - (window.innerWidth / 2 - event.x)
     }px`;
   }
-});
+}
+window.addEventListener("mousemove", moving);
